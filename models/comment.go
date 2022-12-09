b2ac0f576a82c0e1849827f96019e34662b680a7
reinhardjs <reinhardjsilalahi@gmail.com>
2022-12-09 17:42:46 +0700

    chore(my-web-api): refactor comment model

--- models/comment.go
+++ models/comment.go
@@ -1,10 +1,13 @@
 package models
 
-import (
-	"github.com/jinzhu/gorm"
-)
+import "time"
 
 type Comment struct {
-	gorm.Model
-	Comment string `json:"comment"`
+	ID        int       `gorm:"primary_key;auto_increment" json:"id"`
+	PostId    int       `json:"post_id"`
+	CommentId int       `json:"comment_id"`
+	Nickname  string    `json:"nickname"`
+	Content   string    `json:"content"`
+	CreatedAt time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"created_at"`
+	UpdatedAt time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"updated_at"`
 }
