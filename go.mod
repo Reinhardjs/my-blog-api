b1f9814d561940db64a539e86ca1b19e2fd92f03
reinhardjs <reinhardjsilalahi@gmail.com>
2022-12-11 18:13:23 +0700

    chore(my-web-api): added test case CommentUsecase readAll

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.19
 
 require (
 	github.com/garyburd/redigo v1.6.4
+	github.com/go-test/deep v1.1.0
 	github.com/golang/mock v1.6.0
 	github.com/gorilla/mux v1.8.0
 	github.com/jinzhu/gorm v1.9.16
---
+++ usecases/comment_test.go
@@ -0,0 +1,58 @@
+package usecases
+
+import (
+	mocks "dot-crud-redis-go-api/mocks/repositories"
+	"dot-crud-redis-go-api/models"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+)
+
+type Suite struct {
+	suite.Suite
+
+	ctrl       *gomock.Controller
+	repository *mocks.MockCommentRepo
+}
+
+func (s *Suite) SetupSuite() {
+	s.ctrl = gomock.NewController(s.T())
+	s.repository = mocks.NewMockCommentRepo(s.ctrl)
+}
+
+func (s *Suite) AfterTest(_, _ string) {
+	// require.NoError(s.T(), s.mock.ExpectationsWereMet())
+}
+
+func TestInit(t *testing.T) {
+	suite.Run(t, new(Suite))
+}
+
+func (s *Suite) Test_CommentUsecase_ReadAll() {
+	// Arrange
+	s.repository.EXPECT().ReadAll().Return(&[]models.Comment{{
+		ID:      1,
+		Content: "This is content 1",
+	}, {
+		ID:      2,
+		Content: "This is content 2",
+	}}, nil)
+	expectedComments := []models.Comment{
+		{
+			ID:      1,
+			Content: "This is content 1",
+		}, {
+			ID:      2,
+			Content: "This is content 2",
+		},
+	}
+
+	// Act
+	actualComments, err := s.repository.ReadAll()
+
+	// Assert
+	require.NoError(s.T(), err)
+	require.Equal(s.T(), expectedComments, *actualComments)
+}
